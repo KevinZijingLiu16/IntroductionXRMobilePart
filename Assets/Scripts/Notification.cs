8ca65f1162f90d929c15a75adce46429dc761803
ZIJING, LIU <ae0672@student.jamk.fi>
2025-03-01 21:35:01 +0200

    Part 3

--- Assets/Scripts/Notification.cs
+++ Assets/Scripts/Notification.cs
@@ -1,16 +1,38 @@
+using UnityEngine.Android;
+using Unity.Notifications.Android;
 using UnityEngine;
 
 public class Notification : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
-    {
-        
-    }
+    {   // initialize/register the notification channel
+        var channel = new AndroidNotificationChannel()
+        {
+            Id = "channel_id",
+            Name = "Default Channel",
+            Importance = Importance.Default,
+            Description = "Generic notifications",
+        };
+        AndroidNotificationCenter.RegisterNotificationChannel(channel);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+
+        //make sure the App has the permission to send notifications to user
+
+        if(!Permission.HasUserAuthorizedPermission("android.permission.POST_NOTIFICATIONS"))
+        {
+            Permission.RequestUserPermission("android.permission.POST_NOTIFICATIONS");
+        }
+
+        // create and send the notification
+
+        var notification = new AndroidNotification();
+        notification.Title = "This is Notification Title";
+        notification.Text = "HI, this is the nutification text";
+        notification.FireTime = System.DateTime.Now.AddMinutes(1);
+
+        AndroidNotificationCenter.SendNotification(notification, "channel_id");
+
+        notification.LargeIcon = "my_custom_large_icon_id";
+
     }
 }
